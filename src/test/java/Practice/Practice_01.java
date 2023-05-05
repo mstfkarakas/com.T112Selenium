0b3d86b3a718f48df4adddded1ea9442d7c94276
mstfkarakas <mstfkarakas@gmail.com>
2023-05-05 23:52:19 +1000

    ...

---
+++ src/test/java/Practice/Practice_01.java
@@ -0,0 +1,16 @@
+package Practice;
+
+public class Practice_01 {
+    public static void main(String[] args) {
+
+//        1- C01_TekrarTesti isimli bir class olusturun
+//        2- https://www.google.com/ adresine gidin
+//        3- cookies uyarisini kabul ederek kapatin
+//        4- Sayfa basliginin “Google” ifadesi icerdigini test edin
+//        5- Arama cubuguna “Nutella” yazip aratin
+//        6- Bulunan sonuc sayisini yazdirin
+//        7- sonuc sayisinin 10 milyon’dan fazla oldugunu test edin
+//        8- Sayfayi kapatin
+
+    }
+}
